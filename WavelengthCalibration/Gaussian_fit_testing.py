60b277858ac6d2dc5198313b35e073f5ee0077e1
Jason Neal <jason.neal@astro.up.pt>
2015-09-10 23:43:30 +0100

    wavemap begining, difference plot
    linear and quad fit of wavelength map
    difference plot
    added loops to make points equal

--- WavelengthCalibration/Gaussian_fit_testing.py
+++ WavelengthCalibration/Gaussian_fit_testing.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 import scipy.optimize as opt
 
 def onclick(event):
-    global ix, iy
+    global ix, iy, coords
     # Disconnect after right click
     if event.button == 3:
         #print("that was a right mouse click")
@@ -30,83 +30,10 @@ def onclick(event):
     # print 'x = %d, y = %d'%(
     #     ix, iy)
     # assign global variable to access outside of function
-    global coords
+    #global coords
     coords.append((ix, iy))
+    print("Click position", [ix, iy])
     return
-
-#@custom_model
-#def sum_of_gaussians(x, *params):
-    #''' Could also use this to generate the data.
-  #  Has been adjusted from http://docs.astropy.org/en/stable/modeling/new.html
-
-   # x is the x-axis (wavelength or pixel number)  - should be a numpy array
-    #ampltiudes, means asnd sigmas are lists of the values for each gausian we want to fit'''
-    #amplitudes = params[0]
-    #means = params[1]
-    #sigmas = params[2]
-
-    #modeloutput = np.ones_like(x)
-    #for amp, mean, sig in zip(amplitudes, means, sigmas):
-    #    modeloutput -= amp * np.exp(-0.5 * ((x - mean) / sig)**2) # Subtract each gausian
-
-    #return modeloutput
-
-
-def make_mix(numg): 
-    def mix(x, *p):
-        #print("inside mix")
-        ng = numg
-        amplitudes = p[:ng]
-        means = p[ng:2*ng]
-        sigmas = p[2*ng:]
-        modeloutput = np.ones_like(x)
-        print("type model ouput", type(modeloutput))
-        print("type model ouput value", type(modeloutput[0]))
-        print("amplitudes, means, sigmas", amplitudes, means, sigmas)
-        for amp, mean, sig in zip(amplitudes, means, sigmas):
-            #print("inside the mix for loop")
-            #print("this amp", amp, "this mean", mean, "this_sigma", sig)
-            subtraction = amp * np.exp(-0.5 * ((x - mean) / sig)**2)
-            #print("type subtarction",type(subtraction))
-            #print("type subtarction value",type(subtraction[20]))
-            #print(" subtraction ", subtraction)
-            #plt.plot(subtraction, label="subtracted value")
-            #plt.plot(modeloutput, "k.-",label="before subtraction")
-            modeloutput = modeloutput - subtraction # Subtract each gausian
-            #plt.plot(modeloutput, "r.-",label="modeloutput-subtraction")
-            #plt.legend()
-            #plt.show()
-        #print ("modeloutput after for loop", modeloutput)
-        #a = sumarray(gaussian(x,p1),lorentzian(x,p2))
-        #plt.plot(modeloutput)
-        #plt.show()
-        return modeloutput
-    return mix
-
-def mix2(x, numg, *p):
-        print("inside mix 2")
-        ng = numg       # number of gausians
-        amplitudes = p[:ng]
-        means = p[ng:2*ng]
-        sigmas = p[2*ng:]
-        print("number of gausians", ng,"amps", amplitudes, "means", means, "sigmas", sigmas)
-        modeloutput = np.ones_like(x)
-        print("type model ouput", type(modeloutput))
-        for amp, mean, sig in zip(amplitudes, means, sigmas):
-            print("inside the for loop")
-            this_gausian = amp * np.exp(-0.5 * ((x - mean) / sig)**2)
-            print("this_gausian", this_gausian)
-            plt.plot(this_gausian)
-            plt.title("this gausian")
-            plt.show()
-            print("subtraction value", amp * np.exp(-0.5 * ((x - mean) / sig)**2))
-            modeloutput -= amp * np.exp(-0.5 * ((x - mean) / sig)**2) # Subtract each gausian
-            print("subtractin a gausian")
-            #a = sumarray(gaussian(x,p1),lorentzian(x,p2))
-        print("modeloutput",modeloutput)
-        plt.plot(modeloutput)
-        plt.show()
-        return modeloutput
 
 def func(x, *params):
     y = np.ones_like(x)
@@ -116,7 +43,7 @@ def func(x, *params):
         #print("params", params, "length", len(params), "range",range(0, len(params), 3)," i", i)
         ctr = params[i]
         #print("ctr",ctr)
-        amp = params[i+1]
+        amp = abs(params[i+1]) #always positive so peaks are always downward
         #print("amp",amp)
         wid = params[i+2]
         #vert = params[i+3]
@@ -162,16 +89,18 @@ def func(x, *params):
 
 #path = "/home/jneal/Documents/Programming/UsableScripts/WavelengthCalibration/testfiles/"
 path = "/home/jneal/Phd/Codes/Phd-codes/WavelengthCalibration/testfiles/"  # Updated for git repo
-
+path = "C:/Users/Jason/Documents/Phd/Phd-codes/WavelengthCalibration/testfiles/"  # Updated for git repo
+#C:\Users\Jason\Documents\PhD\Phd-codes\WavelengthCalibration\testfiles
 for chip in range(4):
-    coords = []
+   
     #coordsa = []
     #coordsb = []
     UnCalibdata = IOmodule.read_2col(path + "HD30501-1_DRACS_Blaze_Corrected_spectra_chip-" + str(chip + 1) + ".txt")
     Calibdata = IOmodule.read_2col(path + "Telluric_spectra_CRIRES_Chip-" + str(chip + 1) + ".txt")
 
     Goodfit = False # for good line fits
-    while not Goodfit:
+    while True:
+        coords = []
         fig = plt.figure()
         ax1 = fig.add_subplot(111)
         ax2 = ax1.twiny()
@@ -184,35 +113,40 @@ for chip in range(4):
         ax2.set_ylabel('Normalized ADU')
         ax2.set_xlim(np.min(UnCalibdata[0]), np.max(UnCalibdata[0]))
         cid = fig.canvas.mpl_connect('button_press_event', onclick)
-        print("Left click on the maximum of each telluric line peak (Blue) that you want to fit from left to right. \nThen right click to close and perform fit")
+        print("Left click on the maximum of each Spectra line peak (Red) that you want to fit from left to right. \nThen right click to close and perform fit")
         plt.show()
-        print(coords)
+        print("coords found for first plot", coords)
         coords_pxl = coords
-        coords =[]
-
         xpos =[]
         ypos = []
         for tup in coords_pxl:
             xpos.append(tup[0])
             ypos.append(1-tup[1])
 
-        fig = plt.figure()
-        ax1 = fig.add_subplot(111)
-        ax2 = ax1.twiny()
-        ax2.plot(Calibdata[0], Calibdata[1])
-        ax2.set_ylabel('Transmittance')
-        ax2.set_xlabel('Wavelength (nm)')
-        ax2.set_xlim(np.min(Calibdata[0]), np.max(Calibdata[0]))
-        ax1.plot(UnCalibdata[0],UnCalibdata[1],'r')
-        ax1.plot(xpos,np.ones_like(ypos)-ypos,"+k")
-        ax1.set_xlabel('Pixel vals')
-        ax1.set_ylabel('Normalized ADU')
-        ax1.set_xlim(np.min(UnCalibdata[0]), np.max(UnCalibdata[0]))
-        cid = fig.canvas.mpl_connect('button_press_event', onclick)
-        print("Left click on the maximum of each spectral line peak (Red) that you want to select that match the already sellected lines in order from left to right. \nThen right click to close and perform fit")
-        plt.show()
-        print(coords)
-        coords_wl = coords
+        while True:
+            coords =[]
+            fig = plt.figure()
+            ax1 = fig.add_subplot(111)
+            ax2 = ax1.twiny()
+            ax2.plot(Calibdata[0], Calibdata[1])
+            ax2.set_ylabel('Transmittance')
+            ax2.set_xlabel('Wavelength (nm)')
+            ax2.set_xlim(np.min(Calibdata[0]), np.max(Calibdata[0]))
+            ax1.plot(UnCalibdata[0],UnCalibdata[1],'r')
+            ax1.plot(xpos,np.ones_like(ypos)-ypos,"*k", linewidth=5,markersize=10)
+            ax1.set_xlabel('Pixel vals')
+            ax1.set_ylabel('Normalized ADU')
+            ax1.set_xlim(np.min(UnCalibdata[0]), np.max(UnCalibdata[0]))
+            cid = fig.canvas.mpl_connect('button_press_event', onclick)
+            print("Left click on the maximum of each spectral line peak (Red) that you want to select that match the already sellected lines in order from left to right. \nThen right click to close and perform fit")
+            plt.show()
+            print("coords found for second plot", coords)
+            coords_wl = coords
+            print("coords lengths","wl",len(coords_wl), "pxl", len(coords_pxl))
+            #assert len(coords_wl) == len(coords_pxl), " Choosen points were not the same so retry" 
+            if len(coords_wl) == len(coords_pxl):
+                break  # continue on outside while loop
+            
 
         # Calculate the ratios to determine where to try fit gausians
         cal_xpos =[]
@@ -231,7 +165,7 @@ for chip in range(4):
         print("cal y pos", cal_ypos)
 
         #cal_xpos = ratio * (np.max(Calibdata[0])-np.min(Calibdata[0])) + np.min(Calibdata[0])
-        print("calibration xpos cal_xpos", cal_xpos)
+        #print("calibration xpos cal_xpos", cal_xpos)
         """ # cal_xpos and xpos are the xpostions to try fit
         # ypos are the amplitudes
         # sig = 5?
@@ -241,7 +175,8 @@ for chip in range(4):
         init_params_calib = []
         for i in range(len(ypos)):
             init_params_uncalib += [xpos[i], ypos[i], 2]        # center , amplitude, std
-            init_params_calib += [cal_xpos[i], cal_ypos[i], 0.1]    # center , amplitude, std
+        for i in range(len(cal_ypos)):
+            init_params_calib += [cal_xpos[i], cal_ypos[i], 0.04]    # center , amplitude, std
 
         # print("params_uncalib1", params_uncalib1)
         # print("params_calib1", params_calib1)
@@ -282,19 +217,28 @@ for chip in range(4):
             #leastsq_calib, covar_cal = opt.curve_fit(func,Calibdata[0],Calibdata[1], params_calib)
 
         print("fit params individual", fit_params_uncalib, fit_params_calib) #, "covar", covar)
-
-        print("np array length",len(np.array([1,2,3,4])))
+        print("init_params_uncalib", init_params_uncalib)
+      
         Fitted_uncalib = func(UnCalibdata[0], *fit_params_uncalib)
         Fitted_calib = func(Calibdata[0], *fit_params_calib)
-        
-        fig2 = plt.figure()
+        # Guess models used for fitting
+        Guess_uncalib = func(UnCalibdata[0], *init_params_uncalib)
+        Guess_calib = func(Calibdata[0], *init_params_calib)
+
+
+        plt.figure()
+        plt.subplot(211)
         plt.plot(UnCalibdata[0],UnCalibdata[1],'r', label="uncalib")
+        plt.plot(UnCalibdata[0],Guess_uncalib,'go-', label="guess uncalib")
         plt.plot(UnCalibdata[0],Fitted_uncalib,'k.-', label="fitted uncalib")
+        plt.title("Spectral line fits")
         plt.legend()
 
-        fig3 = plt.figure()
+        plt.subplot(212)
         plt.plot(Calibdata[0],Calibdata[1],'b', label="Calib")
+        plt.plot(Calibdata[0],Guess_calib,'go-', label="guess calib")
         plt.plot(Calibdata[0],Fitted_calib,'k.-', label="fitted calib")
+        plt.title("Telluric line fits")
         plt.legend(loc="best")
         print("init params_uncalib", init_params_uncalib)
         print("fit params uncalib", fit_params_uncalib)
@@ -302,9 +246,11 @@ for chip in range(4):
         print("fit params calib", fit_params_calib)
         plt.show()
 
-        Reply = raw_input(" Is this a good fit, y/n?")
-        if Reply.lower() == "y":
-            Goodfit = True
+        #Reply = raw_input(" Is this a good fit, y/n?")
+        Reply = input(" Is this a good fit, y/n?")  #python 3.4
+        if Reply == "y":
+            print("Good fit found")
+            break
         #Goodfit = input(" Is this a good fit")  # python 3
     # after good fit
 
@@ -315,14 +261,38 @@ for chip in range(4):
 
     pixel_pos = fit_params_uncalib[0:-1:3]
     wl_pos = fit_params_calib[0:-1:3]
-    plt.plot(pixel_pos,wl_pos,"rx", markersize=5)
+    plt.plot(pixel_pos,wl_pos,"rx", markersize=5, linewidth=4)
     plt.ylabel("Wavelength")
     plt.xlabel("Pixel position")
 
-    plt.plot([min(pixel_pos), max(pixel_pos)],[min(wl_pos), max(wl_pos)], "k")
+    #plt.plot([min(pixel_pos), max(pixel_pos)],[min(wl_pos), max(wl_pos)], "k")
     # need to fit a linear fit to this from star to end values
-    plt.show()
+   
     # create wavelenght map
 
+    # fit linear
+    linfit = np.polyfit(pixel_pos, wl_pos,1)
+    print("linear fit", linfit)
+    quadfit = np.polyfit(pixel_pos, wl_pos,2)
+    print("quad fit", quadfit)
+    # fit quadratic
 
+    linvals = np.polyval(linfit, range(1,1025))
+    quadvals = np.polyval(quadfit, range(1,1025))
 
+    plt.plot(range(1,1025), linvals , label="linearfit")
+    plt.plot(range(1,1025), quadvals, label="linearfit")
+    plt.legend()
+    print("quad fit vals" , quadvals)
+    plt.show()
+
+    lin_pointvals = np.polyval(linfit, pixel_pos)
+    quad_pointvals = np.polyval(quadfit, pixel_pos) 
+    #plot differences in points from the fits
+
+    plt.plot(pixel_pos,lin_pointvals-wl_pos,"or",label="linfit")
+    plt.plot(pixel_pos,quad_pointvals-wl_pos,"ok",label="quad fit")
+    plt.plot([pixel_pos[0],pixel_pos[-1]], [0,0], 'b--')
+    plt.title("differences between points and fits")
+    plt.legend()
+    plt.show()
--- WavelengthCalibration/IOmodule.py
+++ WavelengthCalibration/IOmodule.py
@@ -5,261 +5,262 @@ def read_col(filename):
 # returns a list in which each sublist correspond to the line's elements.
 # THE RESULT IS A LIST OF STRINGS!
 
-	f = open(filename, "r")
+    f = open(filename, "r")
 
-	list_data = [];
-	
-	while 1: 
-        	line = f.readline()
+    list_data = [];
+    
+    while 1: 
+        line = f.readline()
         
-		if line == "":
-                	break
-		if line[0] == '#':
-                	continue 
+        if line == "":
+                    break
+        if line[0] == '#':
+                    continue 
         
-		line = string.split(line);
-		list_data.append(line);
-		
-	f.close();
-	
-	return list_data
-	
+        #line = string.split(line);
+        line = line.split()
+        list_data.append(line);
+        
+    f.close();
+    
+    return list_data
+    
 def read_2col(filename):
 # The same as the previous, but returns 2 vectors, corresponding each 
 # one to a column.THE RESULTS ARE FLOAT PYTHON VECTORS.
 # Note that in python all "float" are in fact "double-precision".
 
-	list_data = read_col(filename);
-	
-	col1 = [];
-	col2 = [];
-	
-	for i in range(len(list_data)):
-		#checking if the line is valid
-		if(list_data[i][0][0]!='#'):
-			col1.append(float(list_data[i][0]));
-			col2.append(float(list_data[i][1]));
-	
-	return [col1, col2];
-	
+    list_data = read_col(filename);
+    
+    col1 = [];
+    col2 = [];
+    
+    for i in range(len(list_data)):
+        #checking if the line is valid
+        if(list_data[i][0][0]!='#'):
+            col1.append(float(list_data[i][0]));
+            col2.append(float(list_data[i][1]));
+    
+    return [col1, col2];
+    
 def read_3col(filename):
 # The same as the previous, but returns 3 columns
 
-	list_data = read_col(filename);
-	
-	col1 = [];
-	col2 = [];
-	col3 = [];
-	
-	for i in range(len(list_data)):
-		#checking if the line is valid
-		if(list_data[i][0][0]!='#'):
-			col1.append(float(list_data[i][0]));
-			col2.append(float(list_data[i][1]));
-			col3.append(float(list_data[i][2]));
-	
-	return [col1, col2, col3];
+    list_data = read_col(filename);
+    
+    col1 = [];
+    col2 = [];
+    col3 = [];
+    
+    for i in range(len(list_data)):
+        #checking if the line is valid
+        if(list_data[i][0][0]!='#'):
+            col1.append(float(list_data[i][0]));
+            col2.append(float(list_data[i][1]));
+            col3.append(float(list_data[i][2]));
+    
+    return [col1, col2, col3];
 
 def read_4col(filename):
 # The same as the previous, but returns 4 columns
 
-	list_data = read_col(filename);
-	
-	col1 = [];
-	col2 = [];
-	col3 = [];
-	col4 = [];
-	
-	for i in range(len(list_data)):
-		#checking if the line is valid
-		if(list_data[i][0][0]!='#'):
-			col1.append(float(list_data[i][0]));
-			col2.append(float(list_data[i][1]));
-			col3.append(float(list_data[i][2]));
-			col4.append(float(list_data[i][3]));
-	
-	return [col1, col2, col3, col4];
+    list_data = read_col(filename);
+    
+    col1 = [];
+    col2 = [];
+    col3 = [];
+    col4 = [];
+    
+    for i in range(len(list_data)):
+        #checking if the line is valid
+        if(list_data[i][0][0]!='#'):
+            col1.append(float(list_data[i][0]));
+            col2.append(float(list_data[i][1]));
+            col3.append(float(list_data[i][2]));
+            col4.append(float(list_data[i][3]));
+    
+    return [col1, col2, col3, col4];
 
 def read_5col(filename):
 # The same as the previous, but returns 5 columns
 
-	list_data = read_col(filename);
-	
-	col1 = [];
-	col2 = [];
-	col3 = [];
-	col4 = [];
-	col5 = [];
-	
-	for i in range(len(list_data)):
-		#checking if the line is valid
-		if(list_data[i][0][0]!='#'):
-			col1.append(float(list_data[i][0]));
-			col2.append(float(list_data[i][1]));
-			col3.append(float(list_data[i][2]));
-			col4.append(float(list_data[i][3]));
-			col5.append(float(list_data[i][4]));
-			
-	return [col1, col2, col3, col4, col5];	
+    list_data = read_col(filename);
+    
+    col1 = [];
+    col2 = [];
+    col3 = [];
+    col4 = [];
+    col5 = [];
+    
+    for i in range(len(list_data)):
+        #checking if the line is valid
+        if(list_data[i][0][0]!='#'):
+            col1.append(float(list_data[i][0]));
+            col2.append(float(list_data[i][1]));
+            col3.append(float(list_data[i][2]));
+            col4.append(float(list_data[i][3]));
+            col5.append(float(list_data[i][4]));
+            
+    return [col1, col2, col3, col4, col5];    
 
 def read_6col(filename):
 # The same as the previous, but returns 6 columns
 
-	list_data = read_col(filename);
-	
-	col1 = [];
-	col2 = [];
-	col3 = [];
-	col4 = [];
-	col5 = [];
-	col6 = [];
-	
-	for i in range(len(list_data)):
-		#checking if the line is valid
-		if(list_data[i][0][0]!='#'):
-			col1.append(float(list_data[i][0]));
-			col2.append(float(list_data[i][1]));
-			col3.append(float(list_data[i][2]));
-			col4.append(float(list_data[i][3]));
-			col5.append(float(list_data[i][4]));
-			col6.append(float(list_data[i][5]));
-			
-	return [col1, col2, col3, col4, col5, col6];	
+    list_data = read_col(filename);
+    
+    col1 = [];
+    col2 = [];
+    col3 = [];
+    col4 = [];
+    col5 = [];
+    col6 = [];
+    
+    for i in range(len(list_data)):
+        #checking if the line is valid
+        if(list_data[i][0][0]!='#'):
+            col1.append(float(list_data[i][0]));
+            col2.append(float(list_data[i][1]));
+            col3.append(float(list_data[i][2]));
+            col4.append(float(list_data[i][3]));
+            col5.append(float(list_data[i][4]));
+            col6.append(float(list_data[i][5]));
+            
+    return [col1, col2, col3, col4, col5, col6];    
 
 def read_8col(filename):
 # The same as the previous, but returns 8 columns
 
-	list_data = read_col(filename);
-	
-	col1 = [];
-	col2 = [];
-	col3 = [];
-	col4 = [];
-	col5 = [];
-	col6 = [];
-	col7 = [];
-	col8 = [];
-	
-	for i in range(len(list_data)):
-		#checking if the line is valid
-		if(list_data[i][0][0]!='#'):
-			col1.append(float(list_data[i][0]));
-			col2.append(float(list_data[i][1]));
-			col3.append(float(list_data[i][2]));
-			col4.append(float(list_data[i][3]));
-			col5.append(float(list_data[i][4]));
-			col6.append(float(list_data[i][5]));
-			col7.append(float(list_data[i][6]));
-			col8.append(float(list_data[i][7]));
-			
-	return [col1, col2, col3, col4, col5, col6, col7, col8];
+    list_data = read_col(filename);
+    
+    col1 = [];
+    col2 = [];
+    col3 = [];
+    col4 = [];
+    col5 = [];
+    col6 = [];
+    col7 = [];
+    col8 = [];
+    
+    for i in range(len(list_data)):
+        #checking if the line is valid
+        if(list_data[i][0][0]!='#'):
+            col1.append(float(list_data[i][0]));
+            col2.append(float(list_data[i][1]));
+            col3.append(float(list_data[i][2]));
+            col4.append(float(list_data[i][3]));
+            col5.append(float(list_data[i][4]));
+            col6.append(float(list_data[i][5]));
+            col7.append(float(list_data[i][6]));
+            col8.append(float(list_data[i][7]));
+            
+    return [col1, col2, col3, col4, col5, col6, col7, col8];
 
 def read_9col(filename):
 # The same as the previous, but returns 9 columns
 
-	list_data = read_col(filename);
-	
-	col1 = [];
-	col2 = [];
-	col3 = [];
-	col4 = [];
-	col5 = [];
-	col6 = [];
-	col7 = [];
-	col8 = [];
-	col9 = [];
-	
-	for i in range(len(list_data)):
-		#checking if the line is valid
-		if(list_data[i][0][0]!='#'):
-			col1.append(float(list_data[i][0]));
-			col2.append(float(list_data[i][1]));
-			col3.append(float(list_data[i][2]));
-			col4.append(float(list_data[i][3]));
-			col5.append(float(list_data[i][4]));
-			col6.append(float(list_data[i][5]));
-			col7.append(float(list_data[i][6]));
-			col8.append(float(list_data[i][7]));
-			col9.append(float(list_data[i][8]));
-			
-	return [col1, col2, col3, col4, col5, col6, col7, col8, col9];
-	
+    list_data = read_col(filename);
+    
+    col1 = [];
+    col2 = [];
+    col3 = [];
+    col4 = [];
+    col5 = [];
+    col6 = [];
+    col7 = [];
+    col8 = [];
+    col9 = [];
+    
+    for i in range(len(list_data)):
+        #checking if the line is valid
+        if(list_data[i][0][0]!='#'):
+            col1.append(float(list_data[i][0]));
+            col2.append(float(list_data[i][1]));
+            col3.append(float(list_data[i][2]));
+            col4.append(float(list_data[i][3]));
+            col5.append(float(list_data[i][4]));
+            col6.append(float(list_data[i][5]));
+            col7.append(float(list_data[i][6]));
+            col8.append(float(list_data[i][7]));
+            col9.append(float(list_data[i][8]));
+            
+    return [col1, col2, col3, col4, col5, col6, col7, col8, col9];
+    
 def read_10col(filename):
 # The same as the previous, but returns 10 columns
 
-	list_data = read_col(filename);
-	
-	col1 = [];
-	col2 = [];
-	col3 = [];
-	col4 = [];
-	col5 = [];
-	col6 = [];
-	col7 = [];
-	col8 = [];
-	col9 = [];
-	col10 = [];
-	
-	for i in range(len(list_data)):
-		#checking if the line is valid
-		if(list_data[i][0][0]!='#'):
-			col1.append(float(list_data[i][0]));
-			col2.append(float(list_data[i][1]));
-			col3.append(float(list_data[i][2]));
-			col4.append(float(list_data[i][3]));
-			col5.append(float(list_data[i][4]));
-			col6.append(float(list_data[i][5]));
-			col7.append(float(list_data[i][6]));
-			col8.append(float(list_data[i][7]));
-			col9.append(float(list_data[i][8]));
-			col10.append(float(list_data[i][9]));
-			
-	return [col1, col2, col3, col4, col5, col6, col7, col8, col9, col10];
+    list_data = read_col(filename);
+    
+    col1 = [];
+    col2 = [];
+    col3 = [];
+    col4 = [];
+    col5 = [];
+    col6 = [];
+    col7 = [];
+    col8 = [];
+    col9 = [];
+    col10 = [];
+    
+    for i in range(len(list_data)):
+        #checking if the line is valid
+        if(list_data[i][0][0]!='#'):
+            col1.append(float(list_data[i][0]));
+            col2.append(float(list_data[i][1]));
+            col3.append(float(list_data[i][2]));
+            col4.append(float(list_data[i][3]));
+            col5.append(float(list_data[i][4]));
+            col6.append(float(list_data[i][5]));
+            col7.append(float(list_data[i][6]));
+            col8.append(float(list_data[i][7]));
+            col9.append(float(list_data[i][8]));
+            col10.append(float(list_data[i][9]));
+            
+    return [col1, col2, col3, col4, col5, col6, col7, col8, col9, col10];
 
 def read_12col(filename):
 # The same as the previous, but returns 12 columns
 
-	list_data = read_col(filename);
-	
-	col1 = [];
-	col2 = [];
-	col3 = [];
-	col4 = [];
-	col5 = [];
-	col6 = [];
-	col7 = [];
-	col8 = [];
-	col9 = [];
-	col10 = [];
-	col11 = [];
-	col12 = [];
-	
-	for i in range(len(list_data)):
-		#checking if the line is valid
-		if(list_data[i][0][0]!='#'):
-			col1.append(float(list_data[i][0]));
-			col2.append(float(list_data[i][1]));
-			col3.append(float(list_data[i][2]));
-			col4.append(float(list_data[i][3]));
-			col5.append(float(list_data[i][4]));
-			col6.append(float(list_data[i][5]));
-			col7.append(float(list_data[i][6]));
-			col8.append(float(list_data[i][7]));
-			col9.append(float(list_data[i][8]));
-			col10.append(float(list_data[i][9]));
-			col11.append(float(list_data[i][10]));
-			col12.append(float(list_data[i][11]));
-			
-	return [col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12];
+    list_data = read_col(filename);
+    
+    col1 = [];
+    col2 = [];
+    col3 = [];
+    col4 = [];
+    col5 = [];
+    col6 = [];
+    col7 = [];
+    col8 = [];
+    col9 = [];
+    col10 = [];
+    col11 = [];
+    col12 = [];
+    
+    for i in range(len(list_data)):
+        #checking if the line is valid
+        if(list_data[i][0][0]!='#'):
+            col1.append(float(list_data[i][0]));
+            col2.append(float(list_data[i][1]));
+            col3.append(float(list_data[i][2]));
+            col4.append(float(list_data[i][3]));
+            col5.append(float(list_data[i][4]));
+            col6.append(float(list_data[i][5]));
+            col7.append(float(list_data[i][6]));
+            col8.append(float(list_data[i][7]));
+            col9.append(float(list_data[i][8]));
+            col10.append(float(list_data[i][9]));
+            col11.append(float(list_data[i][10]));
+            col12.append(float(list_data[i][11]));
+            
+    return [col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12];
 
 
 def write_2col(filename, data1, data2):
 # Writes data in 2 columns separated by tabs in a "filename" file.
 
-	f = open(filename, "w")
-	
-	for i in range(len(data1)):
-		f.write("\t"+str(data1[i])+"\t\t"+str(data2[i])+"\n")
-	
-	f.close();
-		
+    f = open(filename, "w")
+    
+    for i in range(len(data1)):
+        f.write("\t"+str(data1[i])+"\t\t"+str(data2[i])+"\n")
+    
+    f.close();
+        
