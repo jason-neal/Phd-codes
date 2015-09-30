9f5dc66f6c4124256d2c81252d7ca36717a22222
Jason Neal <jason.neal@astro.up.pt>
2015-09-30 17:11:23 +0100

    Ran this version on all observations today. Added discription to hdr, no need for normobs folder, Tidied up code

--- UsefulCodes/Combine_nod_spectra.py
+++ UsefulCodes/Combine_nod_spectra.py
@@ -41,6 +41,9 @@ def get_filenames(path, regexp, regexp2=False):
 
 def SumNods(Spectra, Headers, Pos="All", Norm="None"):
     """ Add together the nod postitions of Crires spectra"""
+    """ Currently this implements baised on expected nod ordering, 
+    If want to be more sure the correct nods are added the headers
+    should be checked for its nod position"""
     #if Headers[i]["HIERARCH ESO SEQ NODPOS"] == Pos:
     NodSum = np.zeros_like(Spectra[0])
     if Pos.upper() == "ALL":
@@ -60,14 +63,15 @@ def SumNods(Spectra, Headers, Pos="All", Norm="None"):
             NodSum += np.array(Spectra[i])
     if Norm.upper() == "MEDIAN":
       NodSum /= np.median(NodSum)
+    elif Norm.upper() == "MEAN":
+      NodSum /= np.mean(NodSum)  
     elif Norm.upper() == "DIVIDE":
       NodSum /= NodNum  
     return NodSum 
 
 
-
 def ExportToFits(Outputfile, Combined, NodA, NodB, hdr, hdrkeys, hdrvals):
-
+    """ Write Combined DRACS CRIRES NOD Spectra to a fits table file"""
     col1 = fits.Column(name="Combined", format="E", array=Norm_All) # colums of data
     col2 = fits.Column(name="Nod A", format="E", array=NodA)
     col3 = fits.Column(name="Nod B", format="E", array=NodB)
@@ -75,13 +79,10 @@ def ExportToFits(Outputfile, Combined, NodA, NodB, hdr, hdrkeys, hdrvals):
     tbhdu = fits.BinTableHDU.from_columns(cols) # binary tbale hdu
     prihdr = append_hdr(hdr, hdrkeys, hdrvals)
     prihdu = fits.PrimaryHDU(header=prihdr)
-
     thdulist = fits.HDUList([prihdu, tbhdu])
-    print("Writing to fits file")
-    thdulist.writeto(Outputfile,output_verify="fix")   # Fixing errors to work properly
-
+    #print("Writing to fits file")
+    thdulist.writeto(Outputfile,output_verify="silentfix")   # Fixing errors to work properly
     return None
-
 
 
 def append_hdr(hdr, keys, values ,item=0):
@@ -105,49 +106,33 @@ def append_hdr(hdr, keys, values ,item=0):
             print repr(hdr[-2:10])
     return hdr
 
-
-
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Combine CRIRES Nod positions \
                                      Normized values')
     parser.add_argument('inputpath', help='Path to normobs directory')
     #parser.add_argument('-s', '--sun', help='Plot with spectra of the Sun (1)',
     #                     default=False)
-    ##parser.add_argument('-t', '--telluric', help='Plot telluric with spectrum\
-    #                    (1)', default=False)
-    #parser.add_argument('-s', '--stellar', help='Stellar spectra\
-    #                    (1)', default=False)
-    #parser.add_argument('-r', '--rv', help='RV correction to the stellar spectra in\
-    #                    km/s', default=False, type=float)
-    #parser.add_argument('-l', '--lines',
-    #                    help='Lines to plot on top (multiple lines is an\
-    #                    option). If multiple lines needs to be plotted, then\
-    #                    separate with , and without any spaces.', default=False, type=list)
     args = parser.parse_args()
 
-    print args
+    #print args
     path = args.inputpath
-# find norm values in this directory:
+    # find norm values in this directory:
 
     chips = range(4)
-#for chip in chips:
-    while True:
-        chip = 1
+    for chip in chips:
+    #while True:
+        #chip = 1
         org_vals = get_filenames(path, "CRIRE*.ms.fits", "*_" + str(chip + 1) + "*")
         norm_vals = get_filenames(path, "CRIRE*.ms.norm.fits", "*_" + str(chip + 1) + "*")
 
-        print(org_vals)
         I_dracs = []
+        I_dracs_hdrs = []
         I_norm = []
         I_norm_hdrs = []
-        I_dracs_hdrs = []
-        for name in org_vals:    
-                #print("name", name)
-                ThisFile = path + name
-                I_dracs.append(fits.getdata(ThisFile,0))
-                I_dracs_hdrs.append(fits.getheader(ThisFile,0))
+        for name in org_vals:
+            ThisFile = path + name
+            I_dracs.append(fits.getdata(ThisFile,0))
+            I_dracs_hdrs.append(fits.getheader(ThisFile,0))
         
         dracs_All = SumNods(I_dracs, I_dracs_hdrs, Pos="All", Norm="Divide")
         dracs_A = SumNods(I_dracs, I_dracs_hdrs, Pos="A", Norm="Divide")
@@ -162,11 +147,9 @@ if __name__ == '__main__':
         #         I_norm_hdrs.append(fits.getheader(ThisFile))
         #         #print(type(I_norm))
 
-        print(norm_vals)
-        for name in norm_vals:    
-                #print("name", name)
+        #print(norm_vals)
+        for name in norm_vals:
                 ThisFile = path + name
-                #print ("This file = ")
                 ThisNorm = fits.open(ThisFile)
                 Last_normhdr = ThisNorm[0].header
                 I_norm_hdrs.append(Last_normhdr)
@@ -176,10 +159,10 @@ if __name__ == '__main__':
         #print("Inorm",I_norm)
 
         #Last_normhdr.verify()
-
-        Norm_All = SumNods(I_norm, I_norm_hdrs, Pos="All", Norm="Divide")
-        Norm_A = SumNods(I_norm, I_norm_hdrs, Pos="A", Norm="Divide")
-        Norm_B = SumNods(I_norm, I_norm_hdrs, Pos="B", Norm="Divide")
+        NormalizeMethod = "Divide"
+        Norm_All = SumNods(I_norm, I_norm_hdrs, Pos="All", Norm=NormalizeMethod)
+        Norm_A = SumNods(I_norm, I_norm_hdrs, Pos="A", Norm=NormalizeMethod)
+        Norm_B = SumNods(I_norm, I_norm_hdrs, Pos="B", Norm=NormalizeMethod)
 
 #        plt.plot(dracs_All, label="dracs All")
 #        plt.plot(dracs_A, label="dracs A")
@@ -194,17 +177,29 @@ if __name__ == '__main__':
 #        plt.show()
 
         # write ouput to fits file
-        testhdr = fits.Header()
-        testhdr['TESTVal'] = 'Edwin Hubble'
+#        testhdr = fits.Header()
+#        testhdr['TESTVal'] = 'Edwin Hubble'
         T_Now = str(time.gmtime()[0:6])
-        testhdr['Date'] = (T_Now, ' Time fits was last changed')        
-        #fits.writeto(output, hdulist[item].data, hdr, clobber=True)
-        outputfile = path + "test_fits_ouput.fits"
-        #fits.writeto(outputfile, Norm_All, I_norm_hdrs[1])
-        ExportToFits(outputfile,Norm_All,Norm_A,Norm_B,Last_normhdr,["",""],["", ""])
+#        testhdr['Date'] = (T_Now, ' Time fits was last changed')        
+
+        outputfile = path + norm_vals[-1][0:-4]+ "comb.fits"   
+        print("Output file name", outputfile)
+        hdrkeys = ["Discription"]
+        hdrvals = ["Combine DRACS Nomalized CRIRES Nod Spectra"]
+        hdrkeys = ['COMBINEDATE',"COMBINEMETHOD", "COMBNORMALIZE"]
+        hdrvals = [(T_Now, "Time Nods were combined"),("Addition","How nods were added"),(NormalizeMethod, "Method for normalizing combined spectra by divide, mean or median")]
+    
+        for i, name in zip(range(len(org_vals)), org_vals):
+            hdrkeys.append("CRIRES NOD FILE " + str(i + 1))
+            hdrvals.append((name, "Input filename"))
+        ExportToFits(outputfile,Norm_All,Norm_A,Norm_B,Last_normhdr,hdrkeys,hdrvals)
         print("Wrote to fits Succesfully")
 
-        break
+        #break
+
+
+    ## Try load in the combined nod spectra fits files now
+
 
 
 
