c6d4f74a349ce82e5b1adb3bdecd7eb87aea7bdf
Jason Neal <jason.neal@astro.up.pt>
2015-10-27 14:38:10 +0000

    included if__main__ functinality for testing the code works.

--- TelluricSpectra/TellRemoval.py
+++ TelluricSpectra/TellRemoval.py
@@ -9,10 +9,12 @@
 """
 import numpy as np
 import scipy as sp
+import matplotlib.pyplot as plt
+from scipy import interpolate
 
 def divide_spectra(spec_a, spec_b):
-""" Assumes that the spectra have been interpolated to same wavelength step"""
-""" Divide two spectra"""
+    """ Assumes that the spectra have been interpolated to same wavelength step"""
+    """ Divide two spectra"""
     assert(len(spec_a) == len(spec_b)), "Not the same length"
     divide = spec_a / spec_b
     return divide
@@ -21,8 +23,9 @@ def match_wl(wl, spec, ref_wl):
     """Interpolate Wavelengths of spectra to common WL
     Most likely convert telluric to observed spectra wl after wl mapping performed"""
     newspec1 = np.interp(ref_wl, wl, spec)  # 1-d peicewise linear interpolat
-    # cubic spline with scipy   
-    newspec2 = sp.interpolate.interp1d(wl, spec, kind='cubic')(ref_wl)
+    # cubic spline with scipy
+    newspec2 = interpolate.interp1d(wl, spec, kind='cubic')(ref_wl)   
+    #newspec2 = sp.interpolate.interp1d(wl, spec, kind='cubic')(ref_wl)
     return newspec1, newspec2  # test inperpolations 
 
 def plot_spectra(wl, spec, colspec="k.-", label=None, title="Spectrum"):
@@ -32,10 +35,10 @@ def plot_spectra(wl, spec, colspec="k.-", label=None, title="Spectrum"):
     plt.plot(wl, spec, colspec, label=label)
     plt.title(title)
     plt.legend()
-    plt.show(break=False)
+    plt.show(block=False)
     
 def test_plot_interpolation(x1, y1, x2, y2, methodname=None):
-   """ Plotting code """
+    """ Plotting code """
     plt.plot(x1, y1, label="original values")
     plt.plot(x2, y2, label="new points")
     plt.title("testing Interpolation: ", methodname)
@@ -45,23 +48,47 @@ def test_plot_interpolation(x1, y1, x2, y2, methodname=None):
     plt.show(block=False)
 
 
-def TelluricCorrect(wl_obs, spec_obs,wl_tell, spec_tell):
+def TelluricCorrect(wl_obs, spec_obs, wl_tell, spec_tell):
     """Code to contain other functions in this file
 
- 1. Interpolate spectra to same wavelengths with match_WLs()
- 2. Divide by Telluric
-3.   ...
- """
+     1. Interpolate spectra to same wavelengths with match_WLs()
+     2. Divide by Telluric
+     3.   ...
+    """
    
-    interp1, interp2 = match_wls(wl_tell, spec_tell, wl_obs)
+    interp1, interp2 = match_wl(wl_tell, spec_tell, wl_obs)
+    # could just do interp here without  match_wl function 
     # test outputs
-    test_plot_interpolation(wl_tell, spec_tell, wl_obs, interp1)
-    test_plot_interpolation(wl_tell, spec_tell, wl_obs, interp2)
+
+#    test_plot_interpolation(wl_tell, spec_tell, wl_obs, interp1)
+ #   test_plot_interpolation(wl_tell, spec_tell, wl_obs, interp2)
 
     # division
     corrected_spec = divide_spectra(spec_obs, interp2)
-     # 
+    # 
     # other corrections?
     
     
-    return WL, corrected_spec
+    return corrected_spec
+
+
+
+if __name__ == "__main__":
+    """ Some test code for testing functions """
+    sze = 20
+    x2 = range(sze)
+    y2 = np.random.randn(len(x2)) + np.ones_like(x2)
+    y2 = 0.5 * np.ones_like(x2)
+    x1 = np.linspace(1, sze-1.5, 9)
+    y1 = np.random.randn(len(x1)) + np.ones_like(x1)
+    y1 = np.ones_like(x1)
+    print(x1)
+    print(x2)
+    #print(y1)
+    #print(y2)
+    y1_cor = TelluricCorrect(x1, y1, x2, y2)
+    print(x1)
+    print(y1)
+    print(y1_cor)  
+    
+    
