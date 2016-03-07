12bc8d2b42ff29b2ebff4810a37df71f73b4d692
Jason Neal VM <jasonneal13@hotmail.com>
2016-03-07 22:38:36 +0000

    Starting script for analysis for telluric corrected spectra, RV from plot_fits

---
+++ SpectalAnalysis/Analyse_Corrected_Spectra.py
@@ -0,0 +1,22 @@
+" Analyse Telluric Corrected spectra"
+
+from plot_fits import astro_ccf
+#Find RV of host:
+
+# Load Model
+
+
+# Load Spectra
+
+#def ccf_astro(spectrum1, spectrum2, rvmin=-200, rvmax=200, drv=1)
+#    return RV, drvs, cc, drvs, g(drvs)
+
+#spectrum 1 and 2 are (wve,flux) tupples
+astro_ccf()
+
+
+
+
+
+
+# Spectral recovery of companion?
