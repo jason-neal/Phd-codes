584b3463155085d00bff873f8be4b095eea4b81a
Jason Neal <jason.neal@astro.up.pt>
2016-05-06 10:33:32 +0100

    Add convolution code, adapted from perdos codes.
    needs testing though

--- Notebooks/H20_scaling/H20_Scaling_for_Telluric_Correction.py
+++ Notebooks/H20_scaling/H20_Scaling_for_Telluric_Correction.py
@@ -10,7 +10,7 @@
 # Fit to the observed data (Probably with the other lines removed) to fnd the best x to apply for the correction. (Gives flatest result or zero linewidth.) 
 # 
 
-# In[3]:
+# In[4]:
 
 ### Load modules and Bokeh
 # Imports from __future__ in case we're running Python 2
@@ -18,13 +18,11 @@ from __future__ import division, print_function
 from __future__ import absolute_import, unicode_literals
 
 import numpy as np
-
-# Import pyplot for plotting
 import matplotlib.pyplot as plt
+from astropy.io import fits
 
 # Seaborn, useful for graphics
 import seaborn as sns
-
 
 # Magic function to make matplotlib inline; other style specs must come AFTER
 get_ipython().magic('matplotlib inline')
@@ -55,46 +53,292 @@ bokeh.io.output_notebook()
 
 # ### Load in Observed Data
 
-# In[4]:
+# In[5]:
+
+chip1 = "CRIRE.2012-04-07T00-08-29.976_1.nod.ms.norm.sum.wavecal.fits"
+chip2 = "CRIRE.2012-04-07T00-08-29.976_2.nod.ms.norm.sum.wavecal.fits"
+chip3 = "CRIRE.2012-04-07T00-08-29.976_3.nod.ms.norm.sum.wavecal.fits"
+chip4 = "CRIRE.2012-04-07T00-08-29.976_4.nod.ms.norm.sum.wavecal.fits" 
+
+Obs1 = fits.getdata(chip1) 
+hdr1 = fits.getheader(chip1) 
+Obs2 = fits.getdata(chip2) 
+hdr2 = fits.getheader(chip2) 
+Obs3 = fits.getdata(chip3) 
+hdr3 = fits.getheader(chip3) 
+Obs4 = fits.getdata(chip4) 
+hdr4 = fits.getheader(chip4) 
+
+print("Column names = {}".format(Obs1.columns.names))
+wl1 = Obs1["Wavelength"]
+I1_uncorr = Obs1["Extracted_DRACS"]
+
+wl2 = Obs2["Wavelength"]
+I2_uncorr = Obs2["Extracted_DRACS"]
+
+wl3 = Obs3["Wavelength"]
+I3_uncorr = Obs3["Extracted_DRACS"]
+
+wl4 = Obs4["Wavelength"]
+I4_uncorr = Obs4["Extracted_DRACS"]
+
+start_airmass = hdr1["HIERARCH ESO TEL AIRM START"]
+end_airmass = hdr1["HIERARCH ESO TEL AIRM END"]
+obs_airmass = (start_airmass + end_airmass) / 2
+print("Data from Detectors is now loaded")
 
 
+# In[6]:
+
+## Rough berv correction until input calibrated file is calibrated with non berv tapas 
 
 
-# In[ ]:
+# In[7]:
 
-name1 = "CRIRE.2012-04-07T00-08-29.976_1.nod.ms.norm.sum.wavecal.tellcorr.test.fits"
-name2 = "CRIRE.2012-08-01T09-17-30.195_1.nod.ms.norm.sum.wavecal.tellcorr.test.fits"
-name3 = "CRIRE.2012-08-02T08-47-30.843_1.nod.ms.norm.sum.wavecal.tellcorr.test.fits"
-name4 = "CRIRE.2012-08-06T09-42-07.888_1.nod.ms.norm.sum.wavecal.tellcorr.test.fits" 
+wl1 = wl1-.5   #including rough berv correction
+wl2 = wl2-.54  #including rough berv correction
+wl3 = wl3-.55  #including rough berv correction
+wl4 = wl4-.7
 
 
 # ### Load in the tapas data
 
-# In[ ]:
+# In[10]:
 
+import Obtain_Telluric as obt
+tapas_all = "tapas_2012-04-07T00-24-03_ReqId_10_R-50000_sratio-10_barydone-NO.ipac"
+tapas_h20 = "tapas_2012-04-07T00-24-03_ReqId_12_No_Ifunction_barydone-NO.ipac"
+tapas_not_h20 = "tapas_2012-04-07T00-24-03_ReqId_18_R-50000_sratio-10_barydone-NO.ipac"
 
+tapas_all_data, tapas_all_hdr = obt.load_telluric("", tapas_all)
+tapas_all_airmass = float(tapas_all_hdr["airmass"])
+
+print("Telluric Airmass ", tapas_all_airmass)
+tapas_all_respower = int(float((tapas_all_hdr["respower"])))
+print("Telluric Resolution Power =", tapas_all_respower)
+
+tapas_h20_data, tapas_h20_hdr = obt.load_telluric("", tapas_h20)
+tapas_h20_airmass = float(tapas_h20_hdr["airmass"])
+
+print("Telluric Airmass ", tapas_h20_airmass)
+try:
+    tapas_h20_respower = int(float((tapas_h20_hdr["respower"])))
+except:
+    tapas_h20_respower = "Nan"
+print("Telluric Resolution Power =", tapas_h20_respower)
+
+tapas_not_h20_data, tapas_not_h20_hdr = obt.load_telluric("", tapas_not_h20)
+tapas_not_h20_airmass = float(tapas_not_h20_hdr["airmass"])
+
+print("Telluric Airmass ", tapas_not_h20_airmass)
+tapas_not_h20_respower = int(float((tapas_not_h20_hdr["respower"])))
+print("Telluric Resolution Power =", tapas_not_h20_respower)
+    
+    
 
 
 # ### Plot the data
 # Including the 3 tapas models to show they align well and are consistent.
 # 
 
-# In[ ]:
+# In[11]:
 
+plt.plot(wl1, I1_uncorr, 'b') #including rough berv correction
+plt.plot(wl2, I2_uncorr, 'b') #including rough berv correction
+plt.plot(wl3, I3_uncorr, 'b') #including rough berv correction
+plt.plot(wl4, I4_uncorr, 'b') #including rough berv correction
+plt.plot(tapas_h20_data[0], tapas_h20_data[1], "--k", label="H20")
+plt.plot(tapas_all_data[0], tapas_all_data[1], "-r", label="all")
+plt.plot(tapas_not_h20_data[0], tapas_not_h20_data[1], "-.g", label="Not H20")
 
+#plt.legend()
+
+# Make it interactive with Bokeh
+bokeh.plotting.show(bokeh.mpl.to_bokeh())
 
 
 # ### Remove non-H20 lines
 # (Use telluric removal modules)
 # And plot the result.  
 
-# In[ ]:
+# In[12]:
 
 
+from TellRemoval import divide_spectra, airmass_scaling, telluric_correct, match_wl
+
+def correction(wl_obs, spec_obs, wl_tell, spec_tell, obs_airmass, tell_airmass, kind="linear", method="scipy"):
+    interped_tell = match_wl(wl_tell, spec_tell, wl_obs)
+    scaled_tell = airmass_scaling(interped_tell, tell_airmass, obs_airmass)
+    corr_spec = divide_spectra(spec_obs, scaled_tell)                        # Divide by scaled telluric spectra
+    return corr_spec
+    
+#def telluric_correct(wl_obs, spec_obs, wl_tell, spec_tell, obs_airmass, tell_airmass, kind="linear", method="scipy"):
+#    return Corrections, Correction_tells, Correction_Bs, Correction_labels
+
+# Corrections, Correction_tells, Correction_Bs, Correction_labels = telluric_correct(wl2, I2_uncorr, tapas_not_h20[0], tapas_not_h20[1], obs_airmass, tapas_not_h20_airmass) 
+# Getting zero division error from this function so will try it again from te functions here
+tell_airmass = tapas_not_h20_airmass
+
+I1_not_h20_corr = correction(wl1, I1_uncorr, tapas_not_h20_data[0], tapas_not_h20_data[1], obs_airmass, tapas_not_h20_airmass, kind="linear", method="scipy")
+I2_not_h20_corr = correction(wl2, I2_uncorr, tapas_not_h20_data[0], tapas_not_h20_data[1], obs_airmass, tapas_not_h20_airmass, kind="linear", method="scipy")
+I3_not_h20_corr = correction(wl3, I3_uncorr, tapas_not_h20_data[0], tapas_not_h20_data[1], obs_airmass, tapas_not_h20_airmass, kind="linear", method="scipy")
+I4_not_h20_corr = correction(wl4, I4_uncorr, tapas_not_h20_data[0], tapas_not_h20_data[1], obs_airmass, tapas_not_h20_airmass, kind="linear", method="scipy")
+        
+plt.plot(wl1, I1_uncorr, "b")
+plt.plot(wl2, I2_uncorr, "b")
+plt.plot(wl3, I3_uncorr, "b")
+plt.plot(wl4, I4_uncorr, "b")
+plt.plot(wl1, I1_not_h20_corr, "r")
+plt.plot(wl2, I2_not_h20_corr, "r")
+plt.plot(wl3, I3_not_h20_corr, "r")
+plt.plot(wl4, I4_not_h20_corr, "r")
+plt.plot(tapas_not_h20_data[0], tapas_not_h20_data[1], "k")
+# Make it interactive with Bokeh
+bokeh.plotting.show(bokeh.mpl.to_bokeh())
 
 
 # ### Convole instrument profile function:
 # To use inside fit
+
+# In[13]:
+
+def colvolution_function(wl, I, respower):
+    return I_conv
+
+
+# Convolution from NIR analysis
+
+
+# Convolution from Pyastronomy
+from PyAstronomy import pyasl
+
+
+
+# In[14]:
+
+#print(hdr3)
+
+
+# In[23]:
+
+## Convolution from Pedro NIR analysis code
+
+def wav_selector(wav, flux, wav_min, wav_max):
+    """
+    function that returns wavelength and flux withn a giving range
+    """    
+    wav_sel = np.array([value for value in wav if(wav_min < value < wav_max)], dtype="float64")
+    flux_sel = np.array([value[1] for value in zip(wav,flux) if(wav_min < value[0] < wav_max)], dtype="float64")
+    
+    return [wav_sel, flux_sel]
+
+def chip_selector(wav, flux, chip):
+    chip = str(chip)
+    if(chip in ["ALL", "all", "","0"]):
+        chipmin = float(hdr1["HIERARCH ESO INS WLEN STRT1"])  # Wavelength start on detector [nm]
+        chipmax = float(hdr1["HIERARCH ESO INS WLEN END4"])   # Wavelength end on detector [nm]
+        #return [wav, flux]
+    elif(chip == "1"):
+        chipmin = float(hdr1["HIERARCH ESO INS WLEN STRT1"])  # Wavelength start on detector [nm]
+        chipmax = float(hdr1["HIERARCH ESO INS WLEN END1"])   # Wavelength end on detector [nm]
+    elif(chip == "2"):
+        chipmin = float(hdr1["HIERARCH ESO INS WLEN STRT2"])  # Wavelength start on detector [nm]
+        chipmax = float(hdr1["HIERARCH ESO INS WLEN END2"])   # Wavelength end on detector [nm]
+    elif(chip == "3"):   
+        chipmin = float(hdr1["HIERARCH ESO INS WLEN STRT3"])  # Wavelength start on detector [nm]
+        chipmax = float(hdr1["HIERARCH ESO INS WLEN END3"])   # Wavelength end on detector [nm]
+    elif(chip == "4"):   
+        chipmin = float(hdr1["HIERARCH ESO INS WLEN STRT4"])  # Wavelength start on detector [nm]
+        chipmax = float(hdr1["HIERARCH ESO INS WLEN END4"])   # Wavelength end on detector [nm]
+    else:
+        print("Unrecognized chip tag.")
+        exit()
+    
+    #select values form the chip  
+    wav_chip, flux_chip = wav_selector(wav, flux, chipmin, chipmax)
+    
+    return [wav_chip, flux_chip]
+
+def unitary_Gauss(x, center, FWHM):
+    """
+    Gaussian_function of area=1
+	
+	p[0] = A;
+	p[1] = mean;
+	p[2] = FWHM;
+    """
+    
+    sigma = np.abs(FWHM) /( 2 * np.sqrt(2 * np.log(2)) );
+    Amp = 1.0 / (sigma*np.sqrt(2*np.pi))
+    tau = -((x - center)**2) / (2*(sigma**2))
+    result = Amp * np.exp( tau );
+    
+    return result; 	
+
+def convolution_nir(wav, flux, chip, R, FWHM_lim=5.0, plot=True):
+    """Convolution code adapted from pedros code"""
+    
+    wav_chip, flux_chip = chip_selector(wav, flux, chip)
+    #we need to calculate the FWHM at this value in order to set the starting point for the convolution
+    
+    print(wav_chip)
+    print(flux_chip)
+    FWHM_min = wav_chip[0]/R    #FWHM at the extremes of vector
+    FWHM_max = wav_chip[-1]/R       
+    
+    
+    #wide wavelength bin for the resolution_convolution
+    wav_extended, flux_extended = wav_selector(wav, flux, wav_chip[0]-FWHM_lim*FWHM_min, wav_chip[-1]+FWHM_lim*FWHM_max) 
+    wav_extended = np.array(wav_extended, dtype="float64")
+    flux_extended = np.array(flux_extended, dtype="float64")
+    
+    print("Starting the Resolution convolution...")
+    
+    flux_conv_res = []
+    counter=0    
+    for wav in wav_chip:
+        # select all values such that they are within the FWHM limits
+        FWHM = wav/R
+        indexes = [ i for i in range(len(wav_extended)) if ((wav - FWHM_lim*FWHM) < wav_extended[i] < (wav + FWHM_lim*FWHM))]
+        flux_2convolve = flux_extended[indexes[0]:indexes[-1]+1]
+        IP = unitary_Gauss(wav_extended[indexes[0]:indexes[-1]+1], wav, FWHM)
+        flux_conv_res.append(np.sum(IP*flux_2convolve))
+        if(len(flux_conv_res)%(len(wav_chip)/100 ) == 0):
+            counter = counter+1
+            print("Resolution Convolution at %d%%..." % (counter))
+    flux_conv_res = np.array(flux_conv_res, dtype="float64")
+    print("Done.\n")
+    
+    if(plot):
+        fig=plt.figure(1)
+        plt.xlabel(r"wavelength [ $\mu$m ])")
+        plt.ylabel(r"flux [counts] ")
+        plt.plot(wav_chip, flux_chip/np.max(flux_chip), color ='k', linestyle="-", label="Original spectra")
+        plt.plot(wav_chip, flux_conv_res/np.max(flux_conv_res), color ='b', linestyle="-", label="Spectrum observed at and R=%d ." % (R))
+        plt.legend(loc='best')
+        plt.show() 
+    return wav_chip, flux_conv_res
+
+
+# # Test convolution
+
+# In[ ]:
+
+x, y = convolution_nir(tapas_h20_data[0], tapas_h20_data[1], "0", 5000, FWHM_lim=5.0, plot=True)
+  
+
+
+# In[ ]:
+
+plt.plot(tapas_h20_data[0], tapas_h20_data[1],"b")
+plt.plot(x,y, "r")
+plt.xlabel("Wavelength (nm)")
+plt.ylabel("Flux")
+plt.tilte("Test of convolution")
+plt.show()
+# Make it interactive with Bokeh
+#bokeh.plotting.show(bokeh.mpl.to_bokeh())
+
 
 # In[ ]:
 
