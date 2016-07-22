f330c5e63867e46741b20daa6406c3caf005f13a
Jason Neal <jason.neal@astro.up.pt>
2016-07-22 14:24:08 +0000

    Wavelength calibration plot for Azores poster

--- TelluricSpectra/test/test_TelluricRemoval.py
+++ TelluricSpectra/test/test_TelluricRemoval.py
@@ -12,8 +12,8 @@ import numpy as np
 
 
 def test_divide_spectra():
-    assert divide_spectra() 
-    assert divide_spectra(np.array([1,2,1,2,3,1]), np.array([1,2,1,2,3,1])) == np.array(np.array([1,1,1,1,1,1]))
+    assert divide_spectra(np.array(1), np.array(3)) == np.array(1/3)
+    assert all(divide_spectra(np.array([1,2,1,2,3,1]), np.array([1,2,1,2,3,1])) == np.array(np.array([1,1,1,1,1,1])))
 
 
 
@@ -21,16 +21,25 @@ def test_match_wl():
     a = [1, 3, 5, 7, 9]
     b = [2, 4, 6, 8, 10]
     c = [1,2,3,4,5,6,7,8,9]
-    assert match_wl(a,b,c) == [2,3,4,5,6,7,8,9,10]
-    assert match_wl(np.array(a),np.array(b),np.array(c)) == np.array([2,3,4,5,6,7,8,9,10])
+    assert all(match_wl(a,b,c) == [2,3,4,5,6,7,8,9,10])
+    assert all(match_wl(np.array(a),np.array(b),np.array(c)) == np.array([2,3,4,5,6,7,8,9,10]))
 
 
 def test_airmass_scaling():
     """ Test of airmass scaling function"""
-    assert airmass_scaling(np.array([3]), np.array([2]), 1) == 3/2
-    # need mire tests here
+    #print(airmass_scaling(np.array([1,1,1]), 1, 1))
+
+    assert airmass_scaling(np.array([3]), 2, 1) == np.array(3**(1/2))
+    assert all(airmass_scaling(np.array([1,2,3]), 1, 1) == np.array([1,2,3]))
+    assert all(airmass_scaling(np.array([2,2]), 1, 2) == np.array([4,4]))
+    assert all(airmass_scaling(np.array([3,4,5]), 5, 4) == np.array([3,4,5])**(4/5) )# need mire tests here
+
+
+
+
 
 
     if __name__ == "__main__":
         test_divide_spectra()
         test_match_wl()
+        test_airmass_scaling()
