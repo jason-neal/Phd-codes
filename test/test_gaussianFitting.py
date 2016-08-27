55bcd2ac86a76d49407bca0240a96e042783ce63
Jason Neal <jason.neal@astro.up.pt>
2016-08-27 22:55:30 +0100

    Try hypothesis testing failure

---
+++ test/test_gaussianFitting.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import pytest
+
+from hypothesis import given
+import hypothesis.strategies as st
+
+import GaussianFitting as gf 
+
+
+
+@given(st.tuples(st.float(), st.float()), st.float())
+def test_params_and_coords_reversed(coords, delta):
+    # Make sure the x and y coordinates remain the same
+    assert gf.params2coords(gf.coords2gaussian_params(coords, delta))[0,2] == coords
