6e2b2bc687ab3e1955975abaaa08474ef28f07ab
Jason Neal <jason.neal@astro.up.pt>
2017-07-27 12:27:20 +0100

    Add shebangs to new scripts

--- TelluricSpectra/auto_tellcorrect.sh
+++ TelluricSpectra/auto_tellcorrect.sh
@@ -1,8 +1,10 @@
-# Script to run on the wavelenght calibrated spectra.
+#!/usr/bin/env sh
+
+# Script to run on the wavelength calibrated spectra.
 # Does telluric and h20 on all .wavecal.fits files present
 
 # Jason Neal July 2017
 for i in $( ls *.wavecal.fits ); do
-    TellRemoval.py  -e -c -n -t ../Telluric_files/ $i
-    TellRemoval.py  -e -n -t ../Telluric_files/ $i
+    TellRemoval.py -n -e -t ../Telluric_files/ -c $i   # separate constituients
+    TellRemoval.py -n -e -t ../Telluric_files/ $i
     done
