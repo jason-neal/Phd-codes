09745ccaab73f6c03c9063fe17ffdf18e6d0b840
Jason Neal <jason.neal@astro.up.pt>
2017-11-29 12:28:36 +0000

    fix hypothesis call in Faussian fit test

--- test/test_GaussianFitting.py
+++ test/test_GaussianFitting.py
@@ -7,7 +7,7 @@ from hypothesis import given
 import WavelengthCalibration.GaussianFitting as gf
 
 
-@given(st.tuples(st.float(), st.float()), st.float())
+@given(st.tuples(st.floats(), st.floats()), st.floats())
 def test_params_and_coords_reversed(coords, delta):
     # Make sure the x and y coordinates remain the same
     assert gf.params2coords(gf.coords2gaussian_params(coords, delta))[0,2] == coords
